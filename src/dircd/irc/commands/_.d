d77e4a8006838e6a3f95508ddb13dd09ab6b782c
Kyle Clemens <jkc.clemens@gmail.com>
2014-01-08 20:36:48 -0500

    Updated: Use public scope

--- src/dircd/irc/commands/_.d
+++ src/dircd/irc/commands/_.d
@@ -1,19 +1,21 @@
 module dircd.irc.commands._;
 
-public import dircd.irc.commands.CommandHandler;
-public import dircd.irc.commands.ICCap;
-public import dircd.irc.commands.ICIson;
-public import dircd.irc.commands.ICJoin;
-public import dircd.irc.commands.ICMode;
-public import dircd.irc.commands.ICNames;
-public import dircd.irc.commands.ICNick;
-public import dircd.irc.commands.ICNotice;
-public import dircd.irc.commands.ICommand;
-public import dircd.irc.commands.ICPart;
-public import dircd.irc.commands.ICPass;
-public import dircd.irc.commands.ICPong;
-public import dircd.irc.commands.ICPrivmsg;
-public import dircd.irc.commands.ICQuit;
-public import dircd.irc.commands.ICTopic;
-public import dircd.irc.commands.ICUser;
-public import dircd.irc.commands.ICWho;
+public {
+    import dircd.irc.commands.CommandHandler;
+    import dircd.irc.commands.ICCap;
+    import dircd.irc.commands.ICIson;
+    import dircd.irc.commands.ICJoin;
+    import dircd.irc.commands.ICMode;
+    import dircd.irc.commands.ICNames;
+    import dircd.irc.commands.ICNick;
+    import dircd.irc.commands.ICNotice;
+    import dircd.irc.commands.ICommand;
+    import dircd.irc.commands.ICPart;
+    import dircd.irc.commands.ICPass;
+    import dircd.irc.commands.ICPong;
+    import dircd.irc.commands.ICPrivmsg;
+    import dircd.irc.commands.ICQuit;
+    import dircd.irc.commands.ICTopic;
+    import dircd.irc.commands.ICUser;
+    import dircd.irc.commands.ICWho;
+}
